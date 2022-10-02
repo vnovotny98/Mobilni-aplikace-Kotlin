1365e6427be47e831637a991fe368a822501e148
vnovotny <vnovotny@monetplus.cz>
2022-10-02 09:38:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoAdapter: TodoAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        rvToDoItems.adapter = todoAdapter
+        rvToDoItems.layoutManager = LinearLayoutManager( this)
+
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etToDoTitle.text.toString()
+            if(todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etToDoTitle.text.clear()
+            }
+        }
+        btnDeleteDoneToDos.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
+    }
+}
